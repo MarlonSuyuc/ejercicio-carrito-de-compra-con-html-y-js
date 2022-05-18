51ac3096f1d457f4a878fbf881219573ffe49453
marlonsuyuc <marlonsuyuc0@gmail.com>
2022-05-18 16:49:31 -0600

    practicando con app.js

--- app.js
+++ app.js
@@ -3,33 +3,3 @@ const $buttons = document.querySelectorAll(".btn"),
   $template = document.querySelector("#template"),
   fragment = document.createDocumentFragment(),
   carritoObj = {};
-  
-  
-  const agregarFruta = (e) => {
-  const fruta = {
-    id: e.target.dataset.fruta,
-    titulo: e.target.dataset.fruta,
-    cantidad: 1,
-  };
-  if (carritoObj.hasOwnProperty(fruta.titulo)) {
-    fruta.cantidad = carritoObj[fruta.titulo].cantidad + 1;
-  }
-  carritoObj[fruta.titulo] = fruta;
-  console.log(carritoObj);
-  pintarFruta();
-};
-
-const pintarFruta = () => {
-  $carrito.textContent = "";
-  Object.values(carritoObj).forEach((item) => {
-    const clone = $template.content.firstElementChild.cloneNode(true);
-    clone.querySelector(".lead").textContent = item.titulo;
-    clone.querySelector(".badge").textContent = item.cantidad;
-    fragment.appendChild(clone);
-  });
-  $carrito.appendChild(fragment);
-};
-
-$buttons.forEach((btn) => {
-  btn.addEventListener("click", agregarFruta);
-});
